646671c8ae372864b37309590712f2a522783804
lukaszchomatek <lukasz.chomatek@p.lodz.pl>
2021-06-20 16:18:41 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul>
+    <li v-for="(person, index) in users" :key="index">
+      {{person.name + " " + person.surname}}
+    </li>
+  </ul>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      users: null
+    }
+  },
   components: {
     HelloWorld
+  },
+  mounted() {
+    //axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    axios.get('https://ipi-express-demo.herokuapp.com/users')
+    .then(response => this.users = response.data)
   }
 }
 </script>
